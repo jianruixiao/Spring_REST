7cbdfb1e0bcb3aacb2f431824218b4db5a2d9630
jxa12345 <97713792+jxa12345@users.noreply.github.com>
2022-02-26 17:33:27 -0800

    add maven

---
+++ payroll/src/test/java/com/jianrui/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianrui.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
